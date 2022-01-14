6769238efbecb7867f9e4ac806bb28b2de72214b
Ishanthecoder <ishan.f.coutinho@gmail.com>
2022-01-13 16:34:25 -0800

    Copied drivebase code from previous year, removed Controller class

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,3 +1,5 @@
+// Previous code (1/12/2022):
+/*
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -11,6 +13,7 @@ import edu.wpi.first.wpilibj.RobotBase;
  * you are doing, do not modify this file except to change the parameter class to the startRobot
  * call.
  */
+/*
 public final class Main {
   private Main() {}
 
@@ -19,7 +22,20 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
+  /*
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
+}
+*/
+
+// Code from Github (MecanumBot/src/main/java/frc/robot/Main.java) (1/12/2022):
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Main {
+    public static final void main(String[] args) {
+        RobotBase.startRobot(Robot::new);
+    }
 }
