145a74c262e98e8cd3991d74270cbad627bb984b
Iron Riders <software@ironriders.org>
2022-01-13 17:44:05 -0800

    Add Intake Class

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Intake {
+    private CANSparkMax intakeMotor;
+
+    public Intake(int intakeMotorPort) {
+        intakeMotor = new CANSparkMax(intakeMotorPort, MotorType.kBrushless);
+    }
+
+    public void intakeBall() {
+        intakeMotor.set(2);  // TODO: Find good speed
+    }
+
+    public void spitOutBall() {
+        intakeMotor.set(-1);  // TODO: Find good speed
+    }
+
+    public void stop() {
+        intakeMotor.set(0);
+    }
+}
