1e5d293e0dab51983cef02134362058a5ea86e52
Iron Riders <software@ironriders.org>
2022-01-18 17:42:08 -0800

    Switched to latch design instead of belts

--- src/main/java/frc/robot/BallIndexer.java
+++ src/main/java/frc/robot/BallIndexer.java
@@ -1,66 +1,44 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMax; //SparkMax motor controller, not a SparkMax motor
+import com.revrobotics.CANSparkMax; // SparkMax motor controller, not a SparkMax motor
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BallIndexer {
-    // private static final double kSpeedMultiplier = 0.1;
+    private static final int latchMotorChannel = 0;
+    private static final double speed = 1.0;
+    private CANSparkMax motor;
+    private RelativeEncoder encoder;
 
-    // private static final int kFrontLeftChannel = 1;
-    // private static final int kRearLeftChannel = 4;
-    // private static final int kFrontRightChannel = 3;
-    // private static final int kRearRightChannel = 2;
-    // private boolean inverted;
-
-    private static final int leftConveyorChannel = 0;
-    private static final int rightConveyorChannel = 0;
-
-    private static final double speedMultiplier = 1;
-
-    public static final double advanceSpeed = 10;
-    public static final double stopSpeed = 0;
-
-    private CANSparkMax[] motors;
-
-
-    // It is assumed that both motors are for the left and
-    // right conveyor belts, and that there is only one
-    // motor zone
-
-    // private CANSparkMax[] motors;
+    private double startPosition = 0.0; // todo: test to determine value
+    private double endPosition = 1.0; // todo: test to determine value
 
     public BallIndexer() {
-        this.motors = new CANSparkMax[2];
-
-        this.motors[0] = new CANSparkMax(BallIndexer.leftConveyorChannel, MotorType.kBrushless);
-        this.motors[1] = new CANSparkMax(BallIndexer.rightConveyorChannel, MotorType.kBrushless);
-        this.motors[1].setInverted(true); //Switch this to this.motors[0] if it goes backwards
-
-        // TODO: Set up a sensor object that monitors if a ball is in the indexer
-  
+        motor = new CANSparkMax(latchMotorChannel, MotorType.kBrushless);
+        motor.setIdleMode(IdleMode.kBrake);
+        encoder = motor.getEncoder();
     }
 
-    //Methods go here
-
-    public boolean isBallPrepared() {
-        //TODO: Read ball sensor value and return it
-        return true;
+    public void updateDashboard() {
+        SmartDashboard.putNumber("latch encoder position", encoder.getPosition());
     }
 
-    public void setIndexerMechanismSpeed(double speed) {
-        this.motors[0].set(speed * BallIndexer.speedMultiplier);
-    }
-
-    public void advanceBallIfNecessary() {
-        //FUTURE: If necessary, add a timer and decrease the ball's speed slowly if the sudden motor stop causes problems
-        if (this.isBallPrepared()) {
-            this.setIndexerMechanismSpeed(BallIndexer.stopSpeed);
+    public void releaseBall() {
+        if (encoder.getPosition() < endPosition) {
+            motor.set(speed);
         } else {
-            this.setIndexerMechanismSpeed(BallIndexer.advanceSpeed);
+            motor.set(0.0);
         }
     }
 
-
+    public void acceptNextBall() {
+        if (encoder.getPosition() > startPosition) {
+            motor.set(-speed);
+        } else {
+            motor.set(0.0);
+        }
+    }
 }
