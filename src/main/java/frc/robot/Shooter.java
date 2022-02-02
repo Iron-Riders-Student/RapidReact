3354daa6908b99cb0951268709207aa01a08ace9
Iron Riders <software@ironriders.org>
2022-02-01 19:29:31 -0800

    Added ability for motors to have different speeds

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -1,4 +1,5 @@
 package frc.robot;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
@@ -7,19 +8,17 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 public class Shooter {
     private CANSparkMax bottomMotor, topMotor;
-    private RelativeEncoder topEnco,  bottomEnco;
-    private SparkMaxPIDController  topPID, bottomPID;
-    private double kP, kI, kD, kMaxOutput, kMinOutput, tPR /*maxRPM*/;
+    private SparkMaxPIDController topPID, bottomPID;
+    private double kP, kI, kD, kMaxOutput, kMinOutput, tPR /* maxRPM */;
     private final double WHEEL_RADIUS = 0.0;
+    private double topMotorChange = 1.0; // 1.0 is the same, 2.0 is twice as fast
 
-    public Shooter(int bottomPort, int topPort ){
+    public Shooter(int bottomPort, int topPort) {
         bottomMotor = new CANSparkMax(bottomPort, MotorType.kBrushless);
         topMotor = new CANSparkMax(topPort, MotorType.kBrushless);
-        topEnco = topMotor.getEncoder();
-        bottomEnco = bottomMotor.getEncoder();
+        RelativeEncoder bottomEnco = bottomMotor.getEncoder();
         topPID = topMotor.getPIDController();
         bottomPID = bottomMotor.getPIDController();
         tPR = bottomEnco.getCountsPerRevolution();
@@ -27,36 +26,35 @@ public class Shooter {
         kMaxOutput = 1;
         kMinOutput = -1;
         SmartDashboard.putNumber("P Gain", kP);
-        SmartDashboard.putNumber("I  Gain", kI);
-        SmartDashboard.putNumber("D  Gain", kD); 
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("Top Motor Change", topMotorChange);
         topPID.setOutputRange(kMinOutput, kMaxOutput);
         bottomPID.setOutputRange(kMinOutput, kMaxOutput);
         updateNumbers();
     }
 
-    public void stop(){
+    public void stop() {
         topMotor.set(0);
         bottomMotor.set(0);
     }
 
-    public void shoot(double velocity){
-        velocity = (velocity *tPR)/ (2 *Math.PI *WHEEL_RADIUS);
-        topPID.setReference(velocity, ControlType.kVelocity);
+    public void shoot(double velocity) {
+        velocity = (velocity * tPR) / (2 * Math.PI * WHEEL_RADIUS);
+        topPID.setReference(velocity * topMotorChange, ControlType.kVelocity);
         bottomPID.setReference(velocity, ControlType.kVelocity);
     }
 
-
-
-    private void updateNumbers(){
+    private void updateNumbers() {
         kP = SmartDashboard.getNumber("P Gain", kP);
         kI = SmartDashboard.getNumber("I Gain", kI);
         kD = SmartDashboard.getNumber("D Gain", kD);
+        topMotorChange = SmartDashboard.getNumber("Top Motor Change", topMotorChange);
         bottomPID.setP(kP);
         bottomPID.setI(kI);
         bottomPID.setD(kD);
         topPID.setP(kP);
         topPID.setI(kI);
         topPID.setD(kD);
-
     }
 }
