b6ba889549fb69a562d76b90144ecde3d0ac8cf8
Iron Riders <software@ironriders.org>
2022-02-03 18:56:07 -0800

    cleanup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,14 @@
 // Code from Github (MecanumBot/src/main/java/frc/robot/Robot.java) (1/12/2022):
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-    private GenericHID controller;
-    private MecanumDrive mecanumDrive;
     private Vision vision;
 
     @Override
     public void robotInit() {
-        controller = new GenericHID(0);
-        mecanumDrive = new MecanumDrive();
         vision = new Vision();
     }
 
