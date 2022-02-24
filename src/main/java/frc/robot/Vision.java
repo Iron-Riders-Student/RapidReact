d7e02af459fe823a803bafa6e2249e182fe73edd
AG-Coding <anishg2005@gmail.com>
2022-02-24 13:34:07 -0800

    STEERINGASSIST and DISTANCEASSIST WORKS!!!

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -9,12 +9,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Vision {
     private NetworkTable table;
     private PIDController pidController;
+    double[] pidValues;
 
     public Vision() {
         table = NetworkTableInstance.getDefault().getTable("limelight");
-        double[] pidValues = SmartDashboard.getNumberArray("AutoAlign: PID Values", new double[] { 0.02, 0, 0 });
+        pidValues = SmartDashboard.getNumberArray("AutoAlign: PID Values", new double[] { 0.1, 0, 0 });
         pidController = new PIDController(pidValues[0], pidValues[1], pidValues[2]);
+        pidController.setSetpoint(0);
+        SmartDashboard.putNumber("P", pidValues[0]);
         pidController.setTolerance(SmartDashboard.getNumber("AutoAlign: Tolerance", 0.01));
+       // SmartDashboard.putNumber("KP_Dist", Constants.KP_DIST);
+        //SmartDashboard.putNumber("Target_Dist", Constants.TARGET_DIST);
+        //SmartDashboard.putNumber("DIST_MAX_SPEED", Constants.DIST_MAX_SPEED);
+        //SmartDashboard.putNumber("okDistance", Constants.okDistance);
+        SmartDashboard.putNumber("Turn_MIN_ANGLE", 1);
+        SmartDashboard.putNumber("Turn_MAX_Speed", 1);
     }
 
     public double getXAngleOffset() {
@@ -40,14 +49,15 @@ public class Vision {
 
     public double estimateDistance() {
         // final double targetHeight = 102.625;
-        final double targetHeight = 4;
-        final double cameraHeight = 2.626;
-        final double cameraAngleToGround = 0;
+        final double targetHeight = 29;
+        final double cameraHeight = 3.125;
+        final double cameraAngleToGround = -2;
         final double degrees = cameraAngleToGround + getYAngleOffset();
-        // d = (h2-h1) / tan(a1+a2) -
+        SmartDashboard.putNumber("tyAngle", getYAngleOffset());
+        // d = (h2-h1) / tan(a1+a2)
         // https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
         double distance = (targetHeight - cameraHeight) / Math.tan(degrees * Math.PI / 180.0);
-        SmartDashboard.putNumber("DistanceToTarget", distance);
+        //SmartDashboard.putNumber("DistanceToTarget", distance);
         return distance;
     }
 
@@ -63,11 +73,19 @@ public class Vision {
     }
 
     // Adjusts the distance between a vision target and the robot. Uses basic PID
-    // with the ty value from the network table. (Sort of Tested)
+    // with the ty value from the network table. (Tested)
     public double distanceAssist() {
-        double adjustment = !getHasTargets() ? 0.0 : ( estimateDistance() - Constants.TARGET_DIST) * Constants.KP_DIST;
+        //Constants.KP_DIST = SmartDashboard.getNumber("KP", Constants.KP_DIST);
+        //Constants.DIST_MAX_SPEED = SmartDashboard.getNumber("DIST_MAX_SPEED", Constants.DIST_MAX_SPEED);
+        //Constants.TARGET_DIST = SmartDashboard.getNumber("TARGET_DIST", Constants.TARGET_DIST);
+        double Distance_Error = !getHasTargets() ? 0.0 : (estimateDistance() - Constants.TARGET_DIST);
+        double adjustment = Distance_Error * Constants.KP_DIST;
         adjustment = Math.min(Constants.DIST_MAX_SPEED, Math.max(-Constants.DIST_MAX_SPEED, adjustment));
         SmartDashboard.putNumber("Distance Adjustment", adjustment);
+        //Constants.okDistance = SmartDashboard.getNumber("okDistance", Constants.okDistance);
+        if (Math.abs(Distance_Error) < Constants.okDistance) {
+            return 0;
+        }
         return adjustment;
     }
 
@@ -77,14 +95,20 @@ public class Vision {
         if (!getHasTargets() || Math.abs(getXAngleOffset()) < Constants.TURN_MIN_ANGLE) {
             return 0;
         }
+        //Constants.TURN_MAX_SPEED = SmartDashboard.getNumber("TURN_MAX_SPEED",  Constants.TURN_MAX_SPEED);
+        //Constants.TURN_MIN_ANGLE = SmartDashboard.getNumber("TURN_MIN_ANGLE", Constants.TURN_MIN_ANGLE);
+        pidValues[0] = SmartDashboard.getNumber("P", pidValues[0]);
+        pidController.setP(pidValues[0]);
         double adjustment = pidController.calculate(getXAngleOffset());
-        SmartDashboard.putNumber("turningAdjustement", adjustment);
         adjustment = Math.min(Constants.TURN_MAX_SPEED, Math.max(-Constants.TURN_MAX_SPEED, adjustment));
         SmartDashboard.putNumber("Turning Adjustment", adjustment);
-        return adjustment;
+        if (isAligned()) {
+            return 0;
+        }
+        return -adjustment;
     }
 
-    // Checks if alligned (Untested)
+    // Checks if aligned (tested)
     public boolean isAligned() {
         return pidController.atSetpoint();
     }
