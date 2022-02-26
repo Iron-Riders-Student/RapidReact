58eca00a45bdd816ff1420f2eb5c6d64266e3931
AG-Coding <anishg2005@gmail.com>
2022-02-25 20:48:41 -0800

    Added Camera code.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -9,6 +11,9 @@ public class Robot extends TimedRobot {
     public MecanumDrive mecanumDrive;
     public Vision vision;
     public Shooter shooter;
+
+    // Camera Stuff
+    public UsbCamera frontCamera;
 
     @Override
     public void robotInit() {
@@ -20,16 +25,27 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        if (Constants.DRIVER_CONTROL) {
+ if (Constants.DRIVER_CONTROL) {
              if (controller.getRawButtonPressed(1)) {
                 mecanumDrive.invertDrive();
             }
             mecanumDrive.updateSpeed(controller.getRawAxis(0), controller.getRawAxis(1) * controller.getRawAxis(3),
                     controller.getRawAxis(2));
+                    mecanumDrive.updateSpeed(controller.getRawAxis(0), controller.getRawAxis(1), controller.getRawAxis(2));
         } else {
             mecanumDrive.updateSpeed(0, vision.distanceAssist(), vision.steeringAssist());
             vision.updateDashboard();
         }
         SmartDashboard.updateValues();
+    }
+
+    public void TeleopCamera()
+    {
+        // start capture from the camera at the front plugged into USB slot 0
+        frontCamera = CameraServer.getInstance().startAutomaticCapture(0);
+        // set the stream's resolution to 320x240
+        frontCamera.setResolution(320, 240);
+        // set the stream's frames per second to 15
+        frontCamera.setFPS(15);
     }
 }
