ea9af48c216bd2e21f003485057a7580e589e2e7
Alex-Fischman <alexdfischman@gmail.com>
2022-03-01 22:53:20 -0800

    Updated to use servo for indexer

--- src/main/java/frc/robot/BallIndexer.java
+++ src/main/java/frc/robot/BallIndexer.java
@@ -1,44 +1,24 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMax; // SparkMax motor controller, not a SparkMax motor
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BallIndexer {
-    private static final int latchMotorChannel = 0;
-    private static final double speed = 1.0;
-    private CANSparkMax motor;
-    private RelativeEncoder encoder;
-
-    private double startPosition = 0.0; // todo: test to determine value
-    private double endPosition = 1.0; // todo: test to determine value
+    private Servo servo;
 
     public BallIndexer() {
-        motor = new CANSparkMax(latchMotorChannel, MotorType.kBrushless);
-        motor.setIdleMode(IdleMode.kBrake);
-        encoder = motor.getEncoder();
+        servo = new Servo(0);
     }
 
     public void updateDashboard() {
-        SmartDashboard.putNumber("latch encoder position", encoder.getPosition());
+        SmartDashboard.putNumber("indexer position", servo.getAngle());
     }
 
-    public void releaseBall() {
-        if (encoder.getPosition() < endPosition) {
-            motor.set(speed);
-        } else {
-            motor.set(0.0);
-        }
+    public void extend() {
+        servo.setAngle(45);
     }
 
-    public void acceptNextBall() {
-        if (encoder.getPosition() > startPosition) {
-            motor.set(-speed);
-        } else {
-            motor.set(0.0);
-        }
+    public void retract() {
+        servo.setAngle(0);
     }
 }
