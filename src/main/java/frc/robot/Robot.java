9441c2372ecd0084db40f6b3720e7ccba0eae4b3
Iron Riders <software@ironriders.org>
2022-03-10 17:17:03 -0800

    remapped buttons (not tested)and added comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,7 +46,7 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousPeriodic() {
         double timeFromAutoStart = 15.0 - Timer.getMatchTime();
-
+        //The drive conditional where we first go back, then align, then stop
         if (timeFromAutoStart < 3.0) {
             mecanumDrive.updateAutoSpeed(0.0, Constants.DRIVE_SPEED_AUTO, 0.0);
         } else if (timeFromAutoStart < 5.0) {
@@ -55,6 +55,7 @@ public class Robot extends TimedRobot {
             mecanumDrive.updateAutoSpeed(0.0, 0.0, 0.0);
         }
 
+        //This deploys the intake
         if (timeFromAutoStart < 2.0) {
             intake.startDeployment();
         } else {
@@ -62,10 +63,12 @@ public class Robot extends TimedRobot {
             intake.intakeBall();
         }
 
+        //This will start the shooter
         if (timeFromAutoStart > 3.0) {
             shooter.shoot(getClampedRPM());
         }
 
+        //This shoots the ball
         if (timeFromAutoStart < 6.0) {
             // wait for the shooter to get up to speed
         } else if (timeFromAutoStart < 8.0) {
@@ -81,32 +84,39 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+        // toggle limelight on/off
         if (controller.getRawButtonPressed(9)) {
             withLimelight = !withLimelight;
         }
         SmartDashboard.putBoolean("With Limelight", withLimelight);
 
-        if (controller.getRawButtonPressed(4)) {
+        //intake toggle conditional
+        if (controller.getRawButtonPressed(2)) {
             if (intakeState == 1) {
                 intakeState = 0;
             } else {
                 intakeState = 1;
             }
         }
+        //The actual updation part of the intake conditional
         if (intakeState == 1) {
             intake.intakeBall();
         } else {
             intake.stop();
         }
 
-        if (controller.getRawButton(8)) {
+
+        //Reverses the intake
+        if (controller.getRawButton(11)) {
             intake.spitOutBall();
         }
 
+        //This starts the timer for the ball launching 
         if (controller.getRawButtonPressed(1) || controller.getRawButtonPressed(6)) {
             startShootingTime = Timer.getMatchTime();
         }
 
+        //Auto turning and shoot when button 1 is held down.
         if (controller.getRawButton(1)) {
             mecanumDrive.updateAutoSpeed(0, 0, vision.steeringAssist());
             if (withLimelight) {
@@ -118,6 +128,7 @@ public class Robot extends TimedRobot {
                 indexer.extend();
             }
         } else if (controller.getRawButton(6)) {
+            //This is incase we pick up the wrong ball
             shooter.shoot(500);
             if (startShootingTime - Timer.getMatchTime() > 1.5) {
                 indexer.extend();
@@ -127,24 +138,29 @@ public class Robot extends TimedRobot {
             shooter.stop();
         }
 
-        if (controller.getRawButton(16)) {
+        //This starts the deployment by calling the start deploy method
+        if (controller.getRawButton(9)) {
             intake.startDeployment();
         }
-        if (controller.getRawButtonPressed(15)) {
+        //this will finish the deployment after the driver thinks it is deployed.
+        if (controller.getRawButtonPressed(10)) {
             intake.finishDeployment();
         }
 
+        //invert the drive train
         if (controller.getRawButtonPressed(3)) {
             mecanumDrive.invertDrive();
         }
 
+        //Becuase we don't have match time, we use this button to shoot
         if (controller.getRawButton(7)) {
             indexer.extend();
         } else {
             indexer.retract();
         }
 
-        if (controller.getRawButton(2)) {
+        //The first conditional will auto turn if the driver wants, otherwise we will use normal joystick
+        if (controller.getRawButton(8)) {
             mecanumDrive.updateAutoSpeed(0, 0, vision.steeringAssist());
         } else if (!controller.getRawButton(1)) {
             double slider = 0.5 + controller.getRawAxis(2) * 0.5;
@@ -155,6 +171,7 @@ public class Robot extends TimedRobot {
         }
     }
 
+    //This method is used to make it so that the joystic doesn't ghost.
     private double joystickResponse(double raw) {
         double deadband = SmartDashboard.getNumber("deadband", Constants.DEADBAND);
         double deadbanded = 0.0;
